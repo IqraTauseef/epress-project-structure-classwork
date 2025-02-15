b4bb93100f1faa9bb5097bb617b41feda0606faf
IqraTauseef <156837674+IqraTauseef@users.noreply.github.com>
2025-02-15 15:56:41 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+
+// // First route
+// app.get('/', (req, res) => {
+//   res.send('Hello dear!');
+// });
+
+// // Second route
+// app.get('/iqra', (req, res) => {
+//   res.send('Iqra Tauseef');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
+
 const express = require('express');
+const connectDB = require('./db'); // Import DB connection function
+
 const app = express();
-const port = 3000;
 
-// First route
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Example Route
 app.get('/', (req, res) => {
-  res.send('Hello dear!');
+    res.send('MongoDB connected with Express.js');
 });
 
-// Second route
-app.get('/iqra', (req, res) => {
-  res.send('Iqra Tauseef');
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
