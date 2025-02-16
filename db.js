c6dc0fbe3954488d42dc7292eb37ffb7f88973e2
IqraTauseef <156837674+IqraTauseef@users.noreply.github.com>
2025-02-16 12:53:55 +0500

    Add files via upload

--- db.js
+++ db.js
@@ -1,8 +1,24 @@
+// const mongoose = require('mongoose');
+
+// const connectDB = async () => {
+//     try {
+//         await mongoose.connect('mongodb://localhost:27017/mydatabase', {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true
+//         });
+//         console.log('MongoDB connected successfully');
+//     } catch (error) {
+//         console.error('MongoDB connection failed:', error.message);
+//         process.exit(1); // Exit process with failure
+//     }
+// };
+
+// module.exports = connectDB;
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/mydatabase', {
+        await mongoose.connect('mongodb+srv://admin:admin@cluster0.nbe3n.mongodb.net/', {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
@@ -11,6 +27,6 @@ const connectDB = async () => {
         console.error('MongoDB connection failed:', error.message);
         process.exit(1); // Exit process with failure
     }
-};
+}
 
 module.exports = connectDB;
